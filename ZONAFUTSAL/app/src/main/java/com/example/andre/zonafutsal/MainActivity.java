b769dd8787d339acdf8023cd0fe92acc000c51f7
DESKTOP-8CLHHD4\ACER <fajar.MSN10911fcb@gmail.com>
2018-04-24 12:57:48 +0700

    tombol intent di main activity

--- ZONAFUTSAL/app/src/main/java/com/example/andre/zonafutsal/MainActivity.java
+++ ZONAFUTSAL/app/src/main/java/com/example/andre/zonafutsal/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.andre.zonafutsal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button9;
+    private Button button10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adadw;
+
+        button9 = (Button) findViewById(R.id.button9);
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(MainActivity.this, TampilanTekanButtonDetailLapangan.class);
+                startActivity(Intent);
+            }
+        });
+
+        button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Intent = new Intent(MainActivity.this, TampilanUserSudahLogin.class);
+                startActivity(Intent);
+            }
+        });
     }
 }
