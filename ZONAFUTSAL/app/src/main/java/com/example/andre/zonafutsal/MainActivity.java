9c6133124ca8950b60ab874c35643c6a91a5fe3d
DESKTOP-8CLHHD4\ACER <fajar.MSN10911fcb@gmail.com>
2018-05-06 20:27:39 +0700

    no message

--- ZONAFUTSAL/app/src/main/java/com/example/andre/zonafutsal/MainActivity.java
+++ ZONAFUTSAL/app/src/main/java/com/example/andre/zonafutsal/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent Intent = new Intent(MainActivity.this, TampilanButtonDetailLapangan.class);
+                Intent Intent = new Intent(MainActivity.this, DetailLapanganActivity.class);
                 startActivity(Intent);
             }
         });
