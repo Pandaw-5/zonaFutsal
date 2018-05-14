02ce7cfa665e78fac30980bb16040161e4456332
DESKTOP-AI414H6\Andre <arisubay08@gmail.com>
2018-05-14 12:30:56 +0700

    main activity dan fungsi

--- ZONAFUTSAL/app/src/main/java/com/example/andre/zonafutsal/MainActivity.java
+++ ZONAFUTSAL/app/src/main/java/com/example/andre/zonafutsal/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.andre.zonafutsal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,14 +24,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,18 +73,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.beranda) {
+            Intent i =new Intent(getApplicationContext(),MainActivity.class);  startActivity(i);
+        } else if (id == R.id.masuk) {
+            Intent i =new Intent(getApplicationContext(),MasukActivity.class);  startActivity(i);
+        } else if (id == R.id.daftar) {
+            Intent i =new Intent(getApplicationContext(),DaftarActivity.class);  startActivity(i);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
